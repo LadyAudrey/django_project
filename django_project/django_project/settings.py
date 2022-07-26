7af52d5e7602d2eecad05f295bd499391a8f447b
LadyAudrey <theaudreygreene@gmail.com>
2022-07-26 10:39:41 -0700

    Merge remote-tracking branch 'origin/master'

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_project.helloworld.pages.apps.PagesConfig',
+    'helloworld.pages.apps.PageConfig',
 ]
 
 MIDDLEWARE = [
